81e2950c0f0a22259e13a7784defdcffbc09850c
zurich <estebanzurita316@gmail.com>
2018-07-04 23:28:36 -0400

    cambiar pagina principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import { CarListingPage } from '../pages/car-listing/car-listing';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = CarListingPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+// pages
+import { CarListingPage } from '../pages/car-listing/car-listing';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    CarListingPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,7 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    CarListingPage
   ],
   providers: [
     StatusBar,
